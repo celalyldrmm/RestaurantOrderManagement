b2e5fc6e62c60ae40622781cd3e09c629ee1f6e9
Celal Yıldırım <celallee12@gmail.com>
2024-07-14 19:33:23 +0300

    Created Booking,Category,Contact, Discount, Feature, Product, SocialMedia, and Testimonial API controllers.

---
+++ SignalRApi/Controllers/BookingController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.BookingDto;
+using SignalR.EntityLayer.Entities;
+using System.Net.Mail;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        private IBookingService _bookingService;
+
+        public BookingController(IBookingService bookingService)
+        {
+            _bookingService = bookingService;
+        }
+        [HttpGet]
+        public IActionResult BookingList()
+        {
+            var value=_bookingService.TGetListAll();
+            return Ok(value);
+        }
+        [HttpPost]
+        public IActionResult CreateBooking(CreateBookingDto createBookingDto) 
+        {
+            Booking booking = new Booking()
+            {
+                Mail = createBookingDto.Mail,
+                Date = createBookingDto.Date,
+                Name = createBookingDto.Name,
+                PersonCount = createBookingDto.PersonCount,
+                Phone = createBookingDto.Phone
+            };
+            _bookingService.TAdd(booking);
+            return Ok("Rezervasyon başarıyla yapıldı!");
+        }
+        [HttpPut]
+        public IActionResult UpdateBoking(UpdateBookingDto updateBookingDto) 
+        {
+            Booking booking = new Booking()
+            {
+                Mail = updateBookingDto.Mail,
+                Date = updateBookingDto.Date,
+                Name = updateBookingDto.Name,
+                PersonCount = updateBookingDto.PersonCount,
+                Phone = updateBookingDto.Phone
+            };
+            _bookingService.TUpdate(booking);
+            return Ok("Rezervasyon başarıyla güncellendi!");
+        }
+        [HttpDelete]
+        public IActionResult DeleteBooking(int id)
+        {
+            var value = _bookingService.TGetByID(id);
+            _bookingService.TDelete(value);
+            return Ok("Rezervasyonunuz iptal edilmiştir!");
+        }
+        [HttpGet("GetBooking")]
+        public IActionResult GetBooking(int id)
+        {
+            var value = _bookingService.TGetByID(id);
+            return Ok(value);
+        }
+    }
+}
