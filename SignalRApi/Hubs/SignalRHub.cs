64577829f5f069a32d9172a87a89c6fc9786948e
Celal Yıldırım <celallee12@gmail.com>
2024-10-25 21:16:38 +0300

    feat: Added ProgressBarsController and SendProgress method for real-time progress updates - Created ProgressBarsController with an Index action to render the progress bars view. - Implemented SendProgress method to transmit real-time data, including:     - Total money case amount with currency format,     - Active order count,     - Total menu table count.

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -72,7 +72,19 @@ namespace SignalRApi.Hubs
             await Clients.All.SendAsync("ReceiveTMenuTableCount", value16);
 
         }
-        
+
+        public async Task SendProgress()
+        {   
+            var value =_moneyCaseService.TTotalMoneyCaseAmount();
+            await Clients.All.SendAsync("ReciveTTotalMoneyCaseAmount", value.ToString("0.00") + "₺");
+
+            var value2 = _orderService.TActiveOrderCount();
+            await Clients.All.SendAsync("ReceiveActiveOrderCount", value2);
+
+            var value3 = _menuTableService.TMenuTableCount();
+            await Clients.All.SendAsync("ReceiveMenuTableCount", value3);
+        }
+            
        
 
     }
