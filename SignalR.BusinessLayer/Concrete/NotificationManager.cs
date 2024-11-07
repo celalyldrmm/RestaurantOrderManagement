b5494dbaef0a2f589794d4694de78064e5b6fe68
Celal Yıldırım <celallee12@gmail.com>
2024-11-07 14:11:01 +0300

    feat: Added notification functionality
    - Implemented notification management operations in the business layer.
    - Added data access functionality for notifications to handle database interactions.
    - Created `Notification` entity in the entity layer to define the database table structure.
    - Configured necessary migrations to set up the Notification table in the database.

---
+++ SignalR.BusinessLayer/Concrete/NotificationManager.cs
@@ -0,0 +1,45 @@
+﻿using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.DataAccessLayer.EntityFramework;
+using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.BusinessLayer.Concrete
+{
+    public class NotificationManager : INotificationService
+    {
+        private readonly INotificationDal _notificationDal;
+        public NotificationManager(INotificationDal notificationDal)
+        {
+            _notificationDal = notificationDal;
+        }
+        void IGenericService<Notification>.TAdd(Notification entity)
+        {
+            _notificationDal.Add(entity);
+        }
+
+        void IGenericService<Notification>.TDelete(Notification entity)
+        {
+            _notificationDal.Delete(entity);
+        }
+
+        Notification IGenericService<Notification>.TGetByID(int id)
+        {
+           return _notificationDal.GetByID(id);
+        }
+
+        List<Notification> IGenericService<Notification>.TGetListAll()
+        {
+          return  _notificationDal.GetListAll();
+        }
+
+        void IGenericService<Notification>.TUpdate(Notification entity)
+        {
+           _notificationDal.Update(entity);
+        }
+    }
+}
