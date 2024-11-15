4f3e4f50138e50b69a953d492a52f4facffda291
Celal Yıldırım <celallee12@gmail.com>
2024-11-15 20:21:03 +0300

    feat: Implemented message functionality across layers - Added service and manager classes in the business layer to handle message operations. - Created data access interfaces and implementations for message persistence. - Updated database context and added migrations to support the Message entity. - Introduced a Message entity to represent messages in the database.

---
+++ SignalRApi/Controllers/MessageController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.MessageDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        private readonly IMessageService _messageService;
+
+        public MessageController(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
+
+        [HttpGet]
+        public IActionResult MessageList()
+        {
+            var values = _messageService.TGetListAll();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateMessage(CreateMessageDto createMessageDto)
+        {
+            Message message = new Message()
+            {
+                NameSurname = createMessageDto.NameSurname,
+                Mail = createMessageDto.Mail,
+                Phone = createMessageDto.Phone,
+                MessageContent = createMessageDto.MessageContent,
+                MessageSendDate = DateTime.Now,
+                Subject = createMessageDto.Subject,
+                Status = false
+            };
+
+            _messageService.TAdd(message);
+            return Ok("Mesaj başarılı bir şekilde gönderildi!");
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteMessage(int id)
+        {
+            var value = _messageService.TGetByID(id);
+            _messageService.TDelete(value);
+            return Ok("Mesaj başarılı bir şekilde silindi!");
+        }
+        [HttpPut]
+        public IActionResult UpdateMessage(UpdateMessageDto updateMessageDto)
+        {
+            Message message = new Message()
+            {
+                MessageSendDate= updateMessageDto.MessageSendDate,
+                MessageID = updateMessageDto.MessageID,
+                NameSurname = updateMessageDto.NameSurname,
+                Mail = updateMessageDto.Mail,
+                Phone = updateMessageDto.Phone,                
+                MessageContent = updateMessageDto.MessageContent,
+                Subject = updateMessageDto.Subject,
+                Status = false
+            };
+
+            _messageService.TUpdate(message);
+            return Ok("Mesaj başarılı bir şekilde güncellendi! ");
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetMessage(int id)
+        {
+            var value = _messageService.TGetByID(id);
+            return Ok(value);
+
+        }
+    }
+}
